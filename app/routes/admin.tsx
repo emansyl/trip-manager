392e563b30c48f1a7b598f5f5aa4478bfaff1fcd
Emmanuel Sylvester <emmanuel.sylvester22@gmail.com>
2024-10-17 00:52:12 -0400

    Test Pr preview

---
+++ app/routes/admin.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function AdminRoute() {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">Admin Dashboard</h1>
+    </div>
+  );
+}
